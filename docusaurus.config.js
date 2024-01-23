643548daf906f62129f16fbfecc9405ee3d8ce1a
ssrinath-sri <ssrinath.sri@gmail.com>
2024-01-23 11:15:46 +0530

    Just modified title name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'First Dinosaurs Site',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -66,9 +66,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'First Dinosaurs Site',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'First Dinosaurs Site Logo',
           src: 'img/logo.svg',
         },
         items: [
